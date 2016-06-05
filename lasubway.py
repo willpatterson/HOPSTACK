dc7a8033b6da717fe3d27f4c478b9a6d1c1b2aa1
Will Patterson <wpatt2@pdx.edu>
2016-06-04 17:22:47 -0700

    Replaced LAS data object classes with new versions

--- lasubway.py
+++ lasubway.py
@@ -3,8 +3,32 @@ This is a general starting file for LASubway
 This file was formally used to start LASDI
 """
 
-class Metro:
-    def __init__(self, metro_file):
-        base_dir = None
+class LASDataObject(object):
+    """
+    Base object for all other LASDataObjects (metro, metro line, station)
+    """
+    def __init__(self, init_data):
+        self.name = init_data['name']
+        self.base_directory = init_data['base_dir']
+        self.comments = init_data['comments']
+
+class Metro(LASDataObject):
+    """
+    """
+    def __init__(self, init_data):
+        super.__init__(self, init_data)
+        self.required_pacakges = init_data['required_pacakges']
+
+
+class Station(LASDataObject):
+    """
+    """
+    def __init__(self, init_data):
+        super.__init__(self, init_data)
+        self.in_stream = init_data['in']   #TODO Consider multiple streams for one station
+        self.out_stream = init_data['out'] #
+        self.cmd = init_data['cmd']
+
+
 
 
