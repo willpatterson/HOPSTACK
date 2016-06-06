bcb89bfc21cf3866fd8352ceb2b289479c915af6
Will Patterson <wpatt2@pdx.edu>
2016-06-05 22:28:50 -0700

    Added start of the diagram export function

--- lasubway.py
+++ lasubway.py
@@ -1,6 +1,8 @@
 """
 This is a general starting file for LASubway
-This file was formally used to start LASDI
+
+TODO:
+    Get the full path for Base dir in metro
 """
 
 class LASDataObject(object):
@@ -12,12 +14,29 @@ class LASDataObject(object):
         self.base_directory = init_data['base_dir']
         self.comments = init_data['comments']
 
+    def export_diagram(self):
+        """
+        Returns a ascii table of the data object
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    def create_box(width, title, body):
+        """
+        Creates a box with a title body and a specified width
+        """
+        pass #TODO Left off here
+
+
 class Metro(LASDataObject):
     """
     """
     def __init__(self, init_data):
         super.__init__(self, init_data)
         self.required_pacakges = init_data['required_pacakges']
+
+    def load_metro(self):
+        pass
 
 
 class Station(LASDataObject):
