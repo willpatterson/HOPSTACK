1ad2e4e34b7aa0e89f2753a04cb2fc956e175a65
Will Patterson <wpatt2@pdx.edu>
2016-06-10 17:40:57 -0700

    Created setup.py and lasubway dir. Moved py scripts to lasubway

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+Setup script
+"""
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+setup(name="LASubway",
+      version="0.0.0",
+      description="Software Pipelining Suite"
+      license="MIT",
+      author="William Patterson",
+      packages=find_packages(),
+      package_data={} #TODO Fill in
+      install_requires=['PyYAML'],
+      long_description=long_description(),
+      entry_points={}) #TODO Fill in
