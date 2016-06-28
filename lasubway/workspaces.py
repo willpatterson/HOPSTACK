2af3d2db094eacbf5766cced566df25e8b4eb352
Will Patterson <wpatt2@pdx.edu>
2016-06-27 19:05:07 -0700

    Create workspace object file

---
+++ lasubway/workspaces.py
@@ -0,0 +1,15 @@
+"""
+File containing workspace classes
+
+**This file is VERY incomplete**
+"""
+
+class WorkSpace(object):
+    """ """
+    def __init__(self):
+        self.name = ""
+        self.group = ""
+
+    @staticmethod
+    def factory():
+        pass
